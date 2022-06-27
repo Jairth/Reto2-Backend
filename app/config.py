e35de26546cbfde22ce2d68ce7f0a078f5bcc62f
Jairth <rodriguez6191@hotmail.com>
2022-06-26 19:56:18 -0500

    Primer commit

---
+++ app/config.py
@@ -0,0 +1,2 @@
+class Config:
+  SECRET_KEY = 'qwerty321654987'
---
+++ app/firebaseConfig.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyAQVuaMXlB_yOB44C3RZe2aJyll0IEUclQ",
+  "authDomain": "portafolio-7db73.firebaseapp.com",
+  "projectId": "portafolio-7db73",
+  "storageBucket": "portafolio-7db73.appspot.com",
+  "messagingSenderId": "715953988675",
+  "appId": "1:715953988675:web:808ce1466277e95d9120f6",
+  "measurementId": "G-T4FXTLE10X",
+  "databaseURL": ""
+}
